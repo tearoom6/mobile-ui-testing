371f4a37c24027f412a4a3dd47bb0717950dbd5b
Tomohiro Murota <tomohiro.murota@gmail.com>
2017-07-18 03:09:38 +0900

    Create Android project base

---
+++ mobile-ui-testing-android/app/src/main/java/biz/tearoom6/mobile_ui_testing_android/MainActivity.java
@@ -0,0 +1,13 @@
+package biz.tearoom6.mobile_ui_testing_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
